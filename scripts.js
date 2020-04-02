72fcdeba6ad2311a5e7da9e876804f26ac299cd2
User <user@MacBook-Pro.local>
2020-04-02 18:38:29 -0500

    elemrnt changes

--- scripts.js
+++ scripts.js
@@ -7,37 +7,33 @@
     // next to each @TODO you will find tasks that need to be finished
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+]
+
 let currentMarker = 'X'
-
-
-
 
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
-  console.log(`The element you clicked on has an id:  ${element.id}`)
+  console.log(`The element you clicked on has an id:  ${element.id}`);
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
   if(!document.getElementById(element.id).innerHTML){
-    addMarker(element.id)
+    addMarker(element.id);
   }
 }
 
-
-
-
-
-
-
-
-
-
-
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+  board[row][column] = currentMarker
 
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
@@ -46,22 +42,48 @@ const addMarker = (id) => {
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
   console.log(id);
   document.getElementById(id).innerHTML = currentMarker;
+  checkForWin();
+}
+  const checkForWin = () => {
+    if(horizontalWin() || verticalWin() || diagonalWin()) {
+      window.alert(`Player ${currentMarker} won!`);
+    } else {
+changeMarker();
+}
+  }
+ 
+  const horizontalWin = () => {
+    if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")){
+      (window.alert(`Player ${currentMarker} won!`));
+    } if ((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")){
+      (window.alert(`Player ${currentMarker} won!`));
+    } if ((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")){
+      (window.alert(`Player ${currentMarker} won!`));
 
-  // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // = currentMarker
-  // .getElementById(id)
-  // document
-  // .innerHTML 
-
-  changeMarker()
+  }
 }
 
+  const verticalWin = () => {
+    if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")){
+      (window.alert(`Player ${currentMarker} won!`));
+    }
+    if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")){
+      (window.alert(`Player ${currentMarker} won!`));
+    }
+    if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")){
+      (window.alert(`Player ${currentMarker} won!`));
+    }
+  }
+  
 
-
-
-
-
-
+  const diagonalWin = () => {
+    if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")){
+      (window.alert(`Player ${currentMarker} won!`));
+    }
+    if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")){
+      (window.alert(`Player ${currentMarker} won!`));
+    }
+  }
 
 
 
@@ -76,15 +98,9 @@ const changeMarker = () => {
 
 
 
-
-
-
-
-
-
-
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
+  console.log()
  const squares = document.getElementsByTagName("td")
 
   
@@ -106,5 +122,14 @@ const resetBoard = () => {
 
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
-  }  
+  }
+  
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ]
+  currentMarker = 'X'
+
+
 }
